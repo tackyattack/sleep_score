72195f0442d4e04f3c92bf03e5c8b7f08e0a6fbb
Henry Bergin <tackyattack@gmail.com>
2019-03-03 20:02:30 -0600

    Reading epochs of data

--- edf.py
+++ edf.py
@@ -1,6 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+import struct
+
+def twos_comp(val, bits):
+    if (val & (1 << (bits - 1))) != 0: # if sign bit is set e.g., 8bit: 128-255
+        val = val - (1 << bits)        # compute negative value
+    return val                         # return positive value as is
 
 class EDF_file():
     edf_file = ""
@@ -78,9 +84,15 @@ class EDF_file():
             counter = counter + 80
 
         self.samples_per_record = []
+        self.signal_offsets = []
         for i in range(self.number_signals):
-            self.samples_per_record.append(int(chunk[counter:counter + 8]))
+            sample_cnt = int(chunk[counter:counter + 8])
+            self.samples_per_record.append(sample_cnt)
             counter = counter + 8
+            if(i > 0):
+                self.signal_offsets.append(self.signal_offsets[i-1]+sample_cnt)
+            else:
+                self.signal_offsets.append(0)
 
         self.reserved_lower = []
         for i in range(self.number_signals):
@@ -92,26 +104,56 @@ class EDF_file():
             self.data_record_length = self.data_record_length + sample_len
 
 
-    def get_record(self, record_number):
-        file_offset = self.header_bytes + self.data_record_length*(record_number - 1)
-        self.edf_file.seek(file_offset, 0)
-        return self.read_EDF_bytes(self.data_record_length)
+    def get_signal_number(self, name):
+        for i in range(self.number_signals):
+            if name in self.signal_labels[i]:
+                return i
 
-    def get_sample(self, signal_name, record_number):
+    def get_record(self, record_number):
+        record_len_bytes = self.data_record_length*2
+        file_offset = self.header_bytes + record_len_bytes*(record_number)
+        self.edf_file.seek(file_offset, 0)
+        return bytearray(self.read_EDF_bytes(record_len_bytes))
+
+    def get_signal_samples(self, offset, size, signal_name):
+        signal = []
+        signal_number = self.get_signal_number(signal_name)
+        ints_left = size
+        current_record = offset / self.samples_per_record[signal_number]
+        current_offset = offset
+
+        while(ints_left > 0):
+            record = self.get_record(current_record)
+            signal_offset = self.signal_offsets[signal_number]
+            pos = current_offset % self.samples_per_record[signal_number]
+            loops = self.samples_per_record[signal_number] - pos
+            if(loops > ints_left):
+                loops = ints_left
+
+            loops = loops*2 # two bytes per sample
+            i = 0
+            while(i < loops):
+                lo = record[i + pos*2 + signal_offset*2]
+                i = i + 1
+                hi = record[i + pos*2 + signal_offset*2]
+                i = i + 1
+                val = (hi << 8) | lo
+                if (val & 0x8000):
+                    val = -0x10000 + val
+                signal.append(val)
+                ints_left = ints_left - 1
+
+            current_record = current_record + 1
+            current_offset = current_offset + loops/2
+
+        return signal
 
 
 e = EDF_file()
 e.open_EDF("../test_vectors/test_generator_2.edf")
 e.parse_header()
-print(e.get_record(1))
+samples = e.get_signal_samples(0, 500, "ECG")
+#print(samples)
 
-# t = np.arange(0.0, 2.0, 0.01)
-# s = 1 + np.sin(2 * np.pi * t)
-# plt.plot(t, s)
-#
-# plt.xlabel('time (s)')
-# plt.ylabel('voltage (mV)')
-# plt.title('About as simple as it gets, folks')
-# plt.grid(True)
-# plt.savefig("test.png")
-# plt.show()
+plt.plot(samples)
+plt.show()
